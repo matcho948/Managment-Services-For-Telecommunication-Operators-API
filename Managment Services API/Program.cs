693159cd15b16ea88fb8a1c69cb7aebf40de5488
matcho948 <mat.chojnacki33@gmail.com>
2022-03-20 23:03:46 +0100

    fix

--- Managment Services API/Program.cs
+++ Managment Services API/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(option=>option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddScoped<IManagmentServicesRepo,ManagmentServicesRepo>();
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
